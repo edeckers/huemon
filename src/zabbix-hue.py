39763f966f75f3365187af295c713f1ec5c4e4b5
Ely Deckers <e.deckers@gmail.com>
2022-02-25 15:20:54 +0100

    Simplify some discoveries

--- src/zabbix-hue.py
+++ src/zabbix-hue.py
@@ -8,167 +8,145 @@ from urllib.request import urlopen
 
 import config
 
-COMMANDS = ["discover", "light", "sensor", "system", "test"]
+COMMANDS = ["discover", "light", "sensor", "system"]
 HUE_HUB_URL = f"http://{config.IP}/api/{config.KEY}"
 
-TYPE_SENSOR_LIGHT = "ZLLLightLevel"
-TYPE_SENSOR_PRESENCE = "ZLLPresence"
-TYPE_SWITCH = "ZLLSwitch"
 
 def hue_url(relative_url):
   return "/".join([HUE_HUB_URL, relative_url])
 
+
 def get_system_config():
   with urlopen(hue_url("config")) as response:
-     return json.loads(response.read())
+    return json.loads(response.read())
+
 
 def get_lights():
   with urlopen(hue_url("lights")) as response:
-     return list(json.loads(response.read()).values())
+    return list(json.loads(response.read()).values())
+
 
 def get_light(unique_id):
-  return list(filter( \
-      lambda info:info["uniqueid"] == unique_id, \
+  return list(filter(
+      lambda info: info["uniqueid"] == unique_id,
       get_lights()))[0]
+
 
 def get_sensors():
   with urlopen(hue_url("sensors")) as response:
     return list(json.loads(response.read()).values())
 
+
 def get_sensor(device_id):
-  return list(filter( \
-      lambda info:"uniqueid" in info and info["uniqueid"] == device_id, \
+  return list(filter(
+      lambda info: "uniqueid" in info and info["uniqueid"] == device_id,
       get_sensors()))[0]
+
 
 def print_sensor_battery_level(unique_id):
   device = get_sensor(unique_id)
 
-  print (float(device["config"]["battery"]))
+  print(float(device["config"]["battery"]))
+
 
 def print_sensor_light_level(unique_id):
   device = get_sensor(unique_id)
 
-  print (float(device["state"]["lightlevel"]))
+  print(float(device["state"]["lightlevel"]))
+
 
 def print_sensor_presence(unique_id):
   device = get_sensor(unique_id)
 
-  print (int(device["state"]["presence"]))
+  print(int(device["state"]["presence"]))
+
 
 def print_sensor_reachable(unique_id):
   device = get_sensor(unique_id)
 
-  print (int(device["config"]["reachable"]))
+  print(int(device["config"]["reachable"]))
+
 
 def print_sensor_temperature(unique_id):
   device = get_sensor(unique_id)
 
-  print (float(device["state"]["temperature"]/100))
+  print(float(device["state"]["temperature"]/100))
+
 
 def print_light_reachable(unique_id):
   light = get_light(unique_id)
 
-  print (int(light["state"]["reachable"]))
+  print(int(light["state"]["reachable"]))
+
 
 def print_light_status(unique_id):
   light = get_light(unique_id)
 
-  print (int(light["state"]["on"]))
+  print(int(light["state"]["on"]))
+
 
 def print_light_upgrade_available(unique_id):
   light = get_light(unique_id)
 
-  print (int(light["swupdate"]["state"] != "noupdates"))
+  print(int(light["swupdate"]["state"] != "noupdates"))
+
 
 def print_light_version(unique_id):
   light = get_light(unique_id)
 
-  print (light["swversion"])
+  print(light["swversion"])
+
 
 def print_system_upgrade_available():
   config = get_system_config()
 
-  print (int(config["swupdate2"]["state"] != "noupdates"))
+  print(int(config["swupdate2"]["state"] != "noupdates"))
+
 
 def print_system_version():
   config = get_system_config()
 
-  print (config["swversion"])
+  print(config["swversion"])
+
+
+def item_to_discovery(item):
+  return {
+      "{#NAME}": item["name"],
+      "{#UNIQUE_ID}": item["uniqueid"],
+  }
+
+
+def has_state_field(field: str):
+  return lambda item: \
+      "state" in item and \
+      field in item["state"] and \
+      "recycle" not in item
+
+
+def print_array_as_discovery(items):
+  print(json.dumps({"data": reduce(
+      lambda p, item: [*p, item_to_discovery(item)],
+      items,
+      [])}))
+
 
 def print_discover_batteries():
   with urlopen(hue_url("sensors")) as response:
-    batteries = filter(lambda s:"config" in s and "battery" in s["config"], list(json.loads(response.read()).values()))
+    batteries = filter(lambda s: "config" in s and "battery" in s["config"], list(
+        json.loads(response.read()).values()))
 
-    print (json.dumps({"data":reduce( \
-            lambda p, light:[*p, {
-                "{#NAME}": light["name"],
-                "{#UNIQUE_ID}": light["uniqueid"],
-            }], \
-            batteries, \
-            [])}))
+    print_array_as_discovery(batteries)
+
 
 def print_discover_lights():
-    print (json.dumps({"data":reduce( \
-            lambda p, light:[*p, {
-                "{#NAME}": light["name"],
-                "{#UNIQUE_ID}": light["uniqueid"],
-            }], \
-            get_lights(), \
-            [])}))
+  print_array_as_discovery(get_lights())
 
-def print_discover_sensors_light():
-    sensors_light = filter( \
-            lambda sensor:"state" in sensor and "lightlevel" in sensor["state"],
-            get_sensors())
 
-    print (json.dumps({"data":reduce( \
-            lambda p, light:[*p, {
-                "{#NAME}": light["name"],
-                "{#UNIQUE_ID}": light["uniqueid"],
-            }], \
-            sensors_light, \
-            [])}))
+def print_discover_sensors_type(field_name):
+  print_array_as_discovery(filter(
+      has_state_field(field_name),
+      get_sensors()))
 
-def print_discover_sensors_presence():
-    sensors_presence = filter( \
-            lambda sensor:"state" in sensor and "presence" in sensor["state"] and "recycle" not in sensor,
-            get_sensors())
-
-    print (json.dumps({"data":reduce( \
-            lambda p, light:[*p, {
-                "{#NAME}": light["name"],
-                "{#UNIQUE_ID}": light["uniqueid"],
-            }], \
-            sensors_presence, \
-            [])}))
-
-def print_discover_sensors_temperature():
-    sensors_temperature = filter( \
-            lambda sensor:"state" in sensor and "temperature" in sensor["state"],
-            get_sensors())
-
-    print (json.dumps({"data":reduce( \
-            lambda p, light:[*p, {
-                "{#NAME}": light["name"],
-                "{#UNIQUE_ID}": light["uniqueid"],
-            }], \
-            sensors_temperature, \
-            [])}))
-
-def print_discover2():
-  with urlopen(hue_url("sensors")) as response:
-      print (json.loads(response.read()))
-      exit (0)
-      sensors = filter(lambda s:"uniqueid" in s and not "recycle" in s, list(json.loads(response.read()).values()))
-
-      print (sensors)
-      print (json.dumps({"data":reduce( \
-              lambda p, light:[*p, {
-                  "{#NAME}": light["name"],
-                  "{#UNIQUE_ID}": light["uniqueid"],
-              }], \
-              sensors, \
-              [])}))
 
 def handle_discover_command(arguments):
   # if (len(arguments) != 1):
@@ -184,16 +162,17 @@ def handle_discover_command(arguments):
     return
 
   if discovery_type == "sensors:presence":
-    print_discover_sensors_presence()
+    print_discover_sensors_type("presence")
     return
 
   if discovery_type == "sensors:light":
-    print_discover_sensors_light()
+    print_discover_sensors_type("lightlevel")
     return
 
   if discovery_type == "sensors:temperature":
-    print_discover_sensors_temperature()
+    print_discover_sensors_type("temperature")
     return
+
 
 def handle_sensor_command(arguments):
   # if (len(arguments) != 1):
@@ -222,6 +201,7 @@ def handle_sensor_command(arguments):
   if (action == "light:level"):
     print_sensor_light_level(device_id)
     return
+
 
 def handle_light_command(arguments):
   # if (len(arguments) != 1):
@@ -256,45 +236,42 @@ def handle_system_command(arguments):
   action = arguments[0]
 
   if (action == "is_upgrade_available"):
-      print_system_upgrade_available()
-      return
+    print_system_upgrade_available()
+    return
 
   if (action == "version"):
-      print_system_version()
-      return
+    print_system_version()
+    return
+
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        print ("Did not receive enough arguments, expected at least one command argument")
-        exit (1)
+  if len(sys.argv) <= 1:
+    print("Did not receive enough arguments, expected at least one command argument")
+    exit(1)
 
-    command = sys.argv[1]
+  command = sys.argv[1]
 
-    is_valid_command = command in COMMANDS
-    if not is_valid_command:
-        print (f"Unexpected command `{command}`, expected one of {COMMANDS}")
-        exit (1)
+  is_valid_command = command in COMMANDS
+  if not is_valid_command:
+    print(f"Unexpected command `{command}`, expected one of {COMMANDS}")
+    exit(1)
 
+  arguments = sys.argv[2:]
+  if (command == "discover"):
+    handle_discover_command(arguments)
+    exit(0)
 
-    arguments = sys.argv[2:]
-    if (command == "discover"):
-        handle_discover_command(arguments)
-        exit (0)
+  if (command == "light"):
+    handle_light_command(arguments)
+    exit(0)
 
-    if (command == "sensor"):
-        handle_sensor_command(arguments)
-        exit (0)
+  if (command == "sensor"):
+    handle_sensor_command(arguments)
+    exit(0)
 
-    if (command == "light"):
-        handle_light_command(arguments)
-        exit (0)
+  if (command == "system"):
+    handle_system_command(arguments)
+    exit(0)
 
-    if (command == "system"):
-        handle_system_command(arguments)
-        exit (0)
-
-    if (command == "test"):
-        print_discover2()
-        exit (0)
-
-    print ("Something unexpected went wrong")
+  print("Something unexpected went wrong")
+  exit(1)
