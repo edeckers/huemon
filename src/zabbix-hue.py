aa3046df6f935f625040b15193da7382ec892411
Ely Deckers <e.deckers@gmail.com>
2022-02-26 16:45:39 +0100

    Add some logging to Api

--- src/zabbix-hue.py
+++ src/zabbix-hue.py
@@ -40,15 +40,24 @@ class Api(ApiInterface):
 
   def get_system_config(self):
     with urlopen(self.__hub_url("config")) as response:
-      return json.loads(response.read())
+      LOG.debug("Retrieving system config from api")
+      r = json.loads(response.read())
+      LOG.debug("Retrieved system config from api")
+      return r
 
   def get_lights(self):
     with urlopen(self.__hub_url("lights")) as response:
-      return list(json.loads(response.read()).values())
+      LOG.debug("Retrieving lights from api")
+      r = list(json.loads(response.read()).values())
+      LOG.debug("Retrieved lights from api")
+      return r
 
   def get_sensors(self):
     with urlopen(self.__hub_url("sensors")) as response:
-      return list(json.loads(response.read()).values())
+      LOG.debug("Retrieving sensors from api")
+      r = list(json.loads(response.read()).values())
+      LOG.debug("Retrieved sensors from api")
+      return r
 
   def get_batteries(self):
     return list(filter(lambda s: "config" in s and "battery" in s["config"], list(
