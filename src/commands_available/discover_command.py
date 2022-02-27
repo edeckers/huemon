b632cdeebfeec2a475d00701a8c35c5b74415dfa
Ely Deckers <e.deckers@gmail.com>
2022-02-27 15:37:24 +0100

    Refactor to separate discovery classes

--- src/commands_available/discover_command.py
+++ src/commands_available/discover_command.py
@@ -7,29 +7,64 @@ from logger_factory import create_logger
 LOG = create_logger()
 
 
-class Discover:
-  def __init__(self, api: ApiInterface):
-    self.api = api
-
-  def __item_to_discovery(item):
+class Discovery:
+  def _item_to_discovery(item):
     return {
         "{#NAME}": item["name"],
         "{#UNIQUE_ID}": item["uniqueid"],
     }
 
-  def __has_state_field(field: str):
+  def _has_state_field(field: str):
     return lambda item: \
         "state" in item and \
         field in item["state"] and \
         "recycle" not in item
 
-  def __print_array_as_discovery(items):
+  def _print_array_as_discovery(items):
     print(json.dumps({"data": reduce(
-        lambda p, item: [*p, Discover.__item_to_discovery(item)],
+        lambda p, item: [*p, Discovery._item_to_discovery(item)],
         items,
         [])}))
 
-  def __print_discover_sensors_type(api, sensor_type):
+  def name():
+    pass
+
+  def exec(self, arguments=None):
+    pass
+
+
+class BatteriesDiscovery(Discovery):
+  def __init__(self, api: ApiInterface):
+    self.api = api
+
+  def name():
+    return "batteries"
+
+  def exec(self, arguments=None):
+    Discovery._print_array_as_discovery(self.api.get_batteries()),
+
+
+class LightsDiscovery(Discovery):
+  def __init__(self, api: ApiInterface):
+    self.api = api
+
+  def name():
+    return "lights"
+
+  def exec(self, arguments=None):
+    Discovery._print_array_as_discovery(self.api.get_lights()),
+
+
+class SensorsDiscovery(Discovery):
+  def __init__(self, api: ApiInterface):
+    self.api = api
+
+  def name():
+    return "sensors"
+
+  def exec(self, arguments=None):
+    sensor_type, *_ = arguments
+
     LOG.debug(
         "Running `discover sensor:*` command (sensor_type=%s)", sensor_type)
     if sensor_type not in ["presence", "light", "temperature"]:
@@ -37,17 +72,22 @@ class Discover:
           "Received unknown sensor type '%s' for `discover sensor:*` command", sensor_type)
       return
 
-    Discover.__print_array_as_discovery(filter(
-        Discover.__has_state_field(
+    Discovery._print_array_as_discovery(filter(
+        Discovery._has_state_field(
             "lightlevel" if sensor_type == "light" else sensor_type),
-        api.get_sensors()))
+        self.api.get_sensors()))
     LOG.debug(
         "Finished `discover sensor:*` command (sensor_type=%s)", sensor_type)
 
+
+class Discover:
+  def __init__(self, api: ApiInterface):
+    self.api = api
+
   __DISCOVERY_HANDLERS = {
-      "batteries": lambda api, _: Discover.__print_array_as_discovery(api.get_batteries()),
-      "lights": lambda api, _: Discover.__print_array_as_discovery(api.get_lights()),
-      "sensors": __print_discover_sensors_type
+      "batteries": lambda api, _: BatteriesDiscovery(api).exec(),
+      "lights": lambda api, _: LightsDiscovery(api).exec(),
+      "sensors": lambda api, arguments: SensorsDiscovery(api).exec(arguments)
   }
 
   def discover(self, discovery_type):
@@ -60,7 +100,8 @@ class Discover:
           "Received unknown target '%s' for `discover` command", target)
       return
 
-    Discover.__DISCOVERY_HANDLERS[target](self.api, maybe_sub_target)
+    Discover.__DISCOVERY_HANDLERS[target](
+        self.api, [maybe_sub_target] if maybe_sub_target else [])
     LOG.debug(
         "Finished `discover` command (discovery_type=%s)", discovery_type)
 
