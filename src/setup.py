366c5587bf9164eaede59b6b1a8f7975d40d7af8
Ely Deckers <e.deckers@gmail.com>
2022-02-27 17:02:42 +0100

    Add build script

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ setup(
   author="Ely Deckers",
   author_email="noreply@nonono.com",
   description="Monitoring for Philips Hue",
-  long_description=open("../README.md").read(),
+  long_description=open("README").read(),
   name="HueMon",
   packages=["hue_mon"],
   url="https://deckers.io",
