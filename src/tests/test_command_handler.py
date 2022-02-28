e1d4d16882b8e79218919fd2301bff13909693da
Ely Deckers <e.deckers@gmail.com>
2022-02-28 22:20:22 +0100

    Add missing license headers to files

--- src/tests/test_command_handler.py
+++ src/tests/test_command_handler.py
@@ -1,3 +1,8 @@
+# Copyright (c) Ely Deckers.
+#
+# This source code is licensed under the MPL-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
 import unittest
 from unittest.mock import MagicMock, patch
 
