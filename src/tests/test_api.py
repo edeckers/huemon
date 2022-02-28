ec26307d6a1e2535c383ae15f30e9a3b62a7c2f7
Ely Deckers <e.deckers@gmail.com>
2022-02-28 18:56:28 +0100

    Add test for CachedApi

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -8,7 +8,8 @@ from huemon.api_interface import ApiInterface
 from tests.fixtures import FIELD_SYSTEM_VERSION, create_system_config
 
 
-INFINITE_CACHE_VALIDITY_SECONDS = 1_000_000
+CACHE_VALIDITY_INFINITE_SECONDS = 1_000_000
+CACHE_VALIDITY_ZERO_SECONDS = 0
 
 
 class MutableApi(ApiInterface):
@@ -42,7 +43,7 @@ class TestCachedApi(unittest.TestCase):
 
     api = CachedApi(
         mutable_api,
-        INFINITE_CACHE_VALIDITY_SECONDS,
+        CACHE_VALIDITY_INFINITE_SECONDS,
         tempfile.mkdtemp())
 
     system_config_pre_recv = api.get_system_config()
@@ -63,6 +64,46 @@ class TestCachedApi(unittest.TestCase):
         version_pre_recv,
         version_post_recv,
         "Received version before and after mutation should be the same")
+    self.assertNotEqual(
+        version_pre,
+        version_post,
+        "Set versions should differ")
+
+  def test_when_cache_expired_return_new_data(self):
+    mutable_api = MutableApi()
+
+    system_config_pre = create_system_config()
+    version_pre = system_config_pre[FIELD_SYSTEM_VERSION]
+
+    mutable_api.set_system_config(system_config_pre)
+
+    api = CachedApi(
+        mutable_api,
+        CACHE_VALIDITY_ZERO_SECONDS,
+        tempfile.mkdtemp())
+
+    system_config_pre_recv = api.get_system_config()
+    version_pre_recv = system_config_pre_recv[FIELD_SYSTEM_VERSION]
+
+    system_config_post = create_system_config()
+    version_post = system_config_post[FIELD_SYSTEM_VERSION]
+    mutable_api.set_system_config(system_config_post)
+
+    system_config_post_recv = api.get_system_config()
+    version_post_recv = system_config_post_recv[FIELD_SYSTEM_VERSION]
+
+    self.assertEqual(
+        version_pre,
+        version_pre_recv,
+        "Received version should equal set version")
+    self.assertEqual(
+        version_post,
+        version_post_recv,
+        "Received version should equal set version")
+    self.assertNotEqual(
+        version_pre_recv,
+        version_post_recv,
+        "Received version before and after mutation should differ")
     self.assertNotEqual(
         version_pre,
         version_post,
