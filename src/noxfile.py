4e02cd123eb74685fbdf2578d7dd3b615fd21df7
Ely Deckers <e.deckers@gmail.com>
2022-03-04 10:18:19 +0100

    chore: add test reporting

--- src/noxfile.py
+++ src/noxfile.py
@@ -4,4 +4,4 @@ import nox
 @nox.session
 def tests(session):
     session.install("pytest")
-    session.run("pytest")
+    session.run("pytest", "--quiet", "--junitxml=reports/junit-test-results.xml")
