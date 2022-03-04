08751fe03c0814a954e5f17e2b067650836c0a81
Ely Deckers <e.deckers@gmail.com>
2022-03-04 09:32:10 +0100

    ci: add test GitHub workflow

--- src/bin/install.sh
+++ src/bin/install.sh
@@ -5,8 +5,14 @@ function cd_to_source_directory () {
 }
 
 function install_git_hooks () {
-  pre-commit install
-  pre-commit install --hook-type commit-msg
+  p run pre-commit install
+  p run pre-commit install --hook-type commit-msg
+}
+
+function install_poetry () {
+  if [ -z ${POETRY} ]; then
+    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+  fi
 }
 
 function install () {
@@ -18,5 +24,6 @@ cd_to_source_directory
 
 source bin/shared.sh
 
-install_git_hooks
+install_poetry
 install
+install_git_hooks
--- src/bin/shared.sh
+++ src/bin/shared.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-POETRY=`command -v poetry 2> /dev/null`
 INSTALL_STAMP_PATH='.install.stamp'
 
+function poetry_path () {
+  if [ -z ${POETRY} ]; then
+    source $HOME/.poetry/env
+  fi
+
+  echo `command -v poetry 2> /dev/null`
+}
+
 function assert_poetry_exists () {
-  if [ -z ${POETRY} ]; then echo "Poetry could not be found. See https://python-poetry.org/docs/"; exit 2; fi
+  if [ -z `poetry_path` ]; then echo "Poetry could not be found. See https://python-poetry.org/docs/"; exit 2; fi
 }
 
 function p () {
   assert_poetry_exists
-  ${POETRY} ${@}
+  `poetry_path` ${@}
 }
