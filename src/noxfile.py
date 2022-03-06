7f12cb3ccb21647d6c0faced6c40e35825a0f157
Ely Deckers <e.deckers@gmail.com>
2022-03-06 13:19:05 +0100

    chore: add code coverage

--- src/noxfile.py
+++ src/noxfile.py
@@ -1,7 +1,26 @@
 import nox
 
 
-@nox.session
+def __load_poetry_venv(session):
+    # https://stackoverflow.com/a/59904246
+    session.run("poetry", "shell")
+    session.run("poetry", "install")
+
+
+@nox.session(python=False)
 def tests(session):
-    session.install("pytest")
-    session.run("pytest", "--quiet", "--junitxml=reports/junit-test-results.xml")
+    __load_poetry_venv(session)
+
+    try:
+        session.run(
+            "pytest",
+            "--cov",
+            "--cov-config=../pyproject.toml",
+            "--cov-report=html",
+            "--cov-report=xml",
+            "--junitxml=reports/junit-test-results.xml",
+            *session.posargs,
+        )
+    finally:
+        if session.interactive:
+            session.run("coverage", "report")
