a78f20ec19d60aa0b100879f424e310331be2cde
Ely Deckers <e.deckers@gmail.com>
2022-03-06 19:50:43 +0100

    chore: test all light commands

--- src/tests/test_light_command.py
+++ src/tests/test_light_command.py
@@ -17,23 +17,24 @@ CACHE_VALIDITY_INFINITE_SECONDS = 1_000_000
 CACHE_VALIDITY_ZERO_SECONDS = 0
 
 
+SOME_LIGHT_MAC_0 = "SO:ME:LI:GH:TM:AC:00"
+SOME_LIGHT_MAC_1 = "SO:ME:LI:GH:TM:AC:01"
+
+
 class TestLightCommand(unittest.TestCase):
     @patch("builtins.print")
     def test_when_light_exists_return_status(self, mock_print: MagicMock):
-        some_light_mac_0 = "SO:ME:LI:GH:TM:AC:00"
-        some_light_mac_1 = "SO:ME:LI:GH:TM:AC:01"
-
         mutable_api = MutableApi()
         mutable_api.set_lights(
             [
                 {
-                    "uniqueid": some_light_mac_0,
+                    "uniqueid": SOME_LIGHT_MAC_0,
                     "state": {
                         "on": 1,
                     },
                 },
                 {
-                    "uniqueid": some_light_mac_1,
+                    "uniqueid": SOME_LIGHT_MAC_1,
                     "state": {
                         "on": 0,
                     },
@@ -45,9 +46,9 @@ class TestLightCommand(unittest.TestCase):
             create_name_to_command_mapping({}, mutable_api, [LightCommand])
         )
 
-        command_handler.exec("light", [some_light_mac_0, "status"])
+        command_handler.exec("light", [SOME_LIGHT_MAC_0, "status"])
         state_light_0 = read_result(mock_print)
-        command_handler.exec("light", [some_light_mac_1, "status"])
+        command_handler.exec("light", [SOME_LIGHT_MAC_1, "status"])
         state_light_1 = read_result(mock_print)
 
         self.assertEqual(1, state_light_0)
@@ -55,20 +56,17 @@ class TestLightCommand(unittest.TestCase):
 
     @patch("builtins.print")
     def test_when_light_exists_return_is_upgrade_available(self, mock_print: MagicMock):
-        some_light_mac_0 = "SO:ME:LI:GH:TM:AC:00"
-        some_light_mac_1 = "SO:ME:LI:GH:TM:AC:01"
-
         mutable_api = MutableApi()
         mutable_api.set_lights(
             [
                 {
-                    "uniqueid": some_light_mac_0,
+                    "uniqueid": SOME_LIGHT_MAC_0,
                     "swupdate": {
                         "state": "noupdates",
                     },
                 },
                 {
-                    "uniqueid": some_light_mac_1,
+                    "uniqueid": SOME_LIGHT_MAC_1,
                     "swupdate": {
                         "state": "update-available",
                     },
@@ -80,10 +78,68 @@ class TestLightCommand(unittest.TestCase):
             create_name_to_command_mapping({}, mutable_api, [LightCommand])
         )
 
-        command_handler.exec("light", [some_light_mac_0, "is_upgrade_available"])
+        command_handler.exec("light", [SOME_LIGHT_MAC_0, "is_upgrade_available"])
         state_light_0 = read_result(mock_print)
-        command_handler.exec("light", [some_light_mac_1, "is_upgrade_available"])
+        command_handler.exec("light", [SOME_LIGHT_MAC_1, "is_upgrade_available"])
         state_light_1 = read_result(mock_print)
 
         self.assertEqual(0, state_light_0)
         self.assertEqual(1, state_light_1)
+
+    @patch("builtins.print")
+    def test_when_light_exists_return_is_reachable(self, mock_print: MagicMock):
+        mutable_api = MutableApi()
+        mutable_api.set_lights(
+            [
+                {"uniqueid": SOME_LIGHT_MAC_0, "state": {"reachable": 0}},
+                {
+                    "uniqueid": SOME_LIGHT_MAC_1,
+                    "state": {
+                        "reachable": 1,
+                    },
+                },
+            ]
+        )
+
+        command_handler = CommandHandler(
+            create_name_to_command_mapping({}, mutable_api, [LightCommand])
+        )
+
+        command_handler.exec("light", [SOME_LIGHT_MAC_0, "reachable"])
+        state_light_0 = read_result(mock_print)
+        command_handler.exec("light", [SOME_LIGHT_MAC_1, "reachable"])
+        state_light_1 = read_result(mock_print)
+
+        self.assertEqual(0, state_light_0)
+        self.assertEqual(1, state_light_1)
+
+    @patch("builtins.print")
+    def test_when_light_exists_return_version(self, mock_print: MagicMock):
+        some_version_0 = "some_version_0"
+        some_version_1 = "some_version_1"
+
+        mutable_api = MutableApi()
+        mutable_api.set_lights(
+            [
+                {
+                    "uniqueid": SOME_LIGHT_MAC_0,
+                    "swversion": some_version_0,
+                },
+                {
+                    "uniqueid": SOME_LIGHT_MAC_1,
+                    "swversion": some_version_1,
+                },
+            ]
+        )
+
+        command_handler = CommandHandler(
+            create_name_to_command_mapping({}, mutable_api, [LightCommand])
+        )
+
+        command_handler.exec("light", [SOME_LIGHT_MAC_0, "version"])
+        state_light_0 = read_result(mock_print)
+        command_handler.exec("light", [SOME_LIGHT_MAC_1, "version"])
+        state_light_1 = read_result(mock_print)
+
+        self.assertEqual(some_version_0, state_light_0)
+        self.assertEqual(some_version_1, state_light_1)
