6f6ed31a54f3e3e1b4a848b1d4c22c2fe10a3a8a
Ely Deckers <e.deckers@gmail.com>
2022-03-07 11:19:07 +0100

    chore: replace magic wait time with semaphore

--- src/tests/test_api_configuration.py
+++ src/tests/test_api_configuration.py
@@ -7,7 +7,6 @@ import os
 import tempfile
 import threading
 import unittest
-from time import sleep
 
 from huemon.api.api import Api
 from huemon.api.api_factory import create_api, create_hue_hub_url
@@ -36,29 +35,36 @@ class TestApiConfiguration(unittest.TestCase):
         self.assertIsInstance(api, Api)
 
     @staticmethod
-    def __wait(timeout_seconds: int, value: dict):
-        sleep(timeout_seconds)
+    def __wait(mutex: threading.Lock, is_started: threading.Event):
+        is_started.set()
 
-        value["result"] = True
+        with mutex:
+            pass
 
     def test_when_locked_return_none(self):
         (lfd, lock_file_path) = tempfile.mkstemp()
 
-        thread_result = {"result": False}
-        target = lambda: run_locked(
-            lock_file_path, lambda: TestApiConfiguration.__wait(1, thread_result)
-        )
+        mutex = threading.Lock()
 
-        thread0 = threading.Thread(target=target)
+        with mutex:
+            is_started_event = threading.Event()
+            target = lambda: run_locked(
+                lock_file_path,
+                lambda: TestApiConfiguration.__wait(mutex, is_started_event),
+            )
 
-        thread0.start()
-        maybe_true = run_locked(lock_file_path, lambda: True)
-        thread0.join()
+            thread0 = threading.Thread(target=target)
+
+            thread0.start()
+            is_started_event.wait()
+
+            maybe_true = run_locked(lock_file_path, lambda: True)
+
+        thread0.join(5)
 
         os.close(lfd)
 
         self.assertIsNone(maybe_true)
-        self.assertEqual(True, thread_result["result"])
 
     def test_when_locked_raises_exception_return_none(self):
         (lfd, lock_file_path) = tempfile.mkstemp()
