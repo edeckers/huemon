7459c3133bfead711b9180e90e3df106c4c3aca5
Ely Deckers <e.deckers@gmail.com>
2022-03-08 12:28:05 +0100

    chore: add dynamic routes for all commands

--- src/huemon/server.py
+++ src/huemon/server.py
@@ -1,9 +1,8 @@
 import contextlib
 import io
-import json
 from typing import List
 
-from fastapi import FastAPI, HTTPException, Query, status
+from fastapi import FastAPI, HTTPException, Query, Response, status
 
 from huemon.commands.command_handler import create_default_command_handler
 from huemon.infrastructure.config_factory import create_config
@@ -15,26 +14,37 @@ app = FastAPI()
 CONFIG = create_config()
 LOG = create_logger()
 
+command_handler = create_default_command_handler(
+    CONFIG, get_commands_path(CONFIG, "enabled")
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+def create_command_route_handler(command_name: str):
+    empty_query = Query([])
+
+    def handle_command_route(
+        q: List[str] = empty_query,
+    ):  # pylint: disable=invalid-name
+        context_reader = io.StringIO()
+        with contextlib.redirect_stdout(context_reader):
+            try:
+                command_handler.exec(command_name, q)
+            except SystemExit as system_exit:
+                raise HTTPException(
+                    detail=context_reader.getvalue(),
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                ) from system_exit
+
+        return Response(context_reader.getvalue(), media_type="plain/text")
+
+    return handle_command_route
 
 
-@app.get("/discover")
-async def discover(q: List[str] = Query(None)):  # pylint: disable=invalid-name
-    command_handler = create_default_command_handler(
-        CONFIG, get_commands_path(CONFIG, "enabled")
-    )
+def __init__():
+    for command_name in command_handler.available_commands():
+        app.add_api_route(
+            f"/{command_name}", create_command_route_handler(command_name)
+        )
 
-    context_reader = io.StringIO()
-    with contextlib.redirect_stdout(context_reader):
-        try:
-            command_handler.exec("discover", q)
-        except SystemExit as system_exit:
-            raise HTTPException(
-                detail=context_reader.getvalue(),
-                status_code=status.HTTP_400_BAD_REQUEST,
-            ) from system_exit
 
-    return json.loads(context_reader.getvalue())
+__init__()
