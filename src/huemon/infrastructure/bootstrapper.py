441909ca1666b1c5def2a461f942a39ed3f9cd2e
Ely Deckers <e.deckers@gmail.com>
2022-03-08 16:06:53 +0100

    chore: add license headers

--- src/huemon/infrastructure/bootstrapper.py
+++ src/huemon/infrastructure/bootstrapper.py
@@ -1,3 +1,8 @@
+# Copyright (c) Ely Deckers.
+#
+# This source code is licensed under the MPL-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
 from huemon.infrastructure.logger_factory import create_logger
 from huemon.infrastructure.urllib_safe_opener import (
     security_urllib_allow_http_and_https_schemas_only,
