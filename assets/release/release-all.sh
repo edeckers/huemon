aeb8a9645345890fcd4e239a824ee3304de39c5d
Ely Deckers <e.deckers@gmail.com>
2022-03-10 15:32:22 +0100

    ci: add release workflow

--- assets/release/release-all.sh
+++ assets/release/release-all.sh
@@ -6,15 +6,33 @@ function cd_to_root_directory () {
 
 function publish_semantic_release () {
   echo "Publishing release"
-  poetry run semantic-release publish
+  publish_result=`p run semantic-release publish 2>&1`
+  error_code=`echo $?`
+  is_error_code=`[ -z ${error_code} ] && echo "true" || echo "false"`
+  
+  no_release_count=`echo "${publish_result}" | grep -cim1 "no release"`
+  is_release=`[ -z ${no_release_count} ] && echo "true" || echo "false"`
+
+  echo "${publish_result}"
+
+  if [[ ${is_error_code} || !${is_release} ]]; then
+    echo "has error code: ${is_error_code}"
+    echo "is release created: ${is_release}"
+
+    echo "Received error code or no release was created. Aborting."
+    exit 0
+  fi
+
   echo "Published release"
 }
 
 function build_dist () {
-  poetry build
+  p build
 }
 
 cd_to_root_directory
+
+source src/bin/shared.sh
 
 publish_semantic_release
 
