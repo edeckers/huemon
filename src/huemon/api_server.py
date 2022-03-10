3a1edbe75920f677be8e7f72588680e0349788b9
Ely Deckers <e.deckers@gmail.com>
2022-03-10 13:21:04 +0100

    fix: reads from other thread stdout (#44)

--- src/huemon/api_server.py
+++ src/huemon/api_server.py
@@ -5,6 +5,7 @@
 
 import contextlib
 import io
+import threading
 from typing import List
 
 from fastapi import FastAPI, HTTPException, Query, Response, status
@@ -17,6 +18,8 @@ LOG = create_logger()
 
 
 class HuemonServerFactory:  # pylint: disable=too-few-public-methods
+    __stdout_reader_mutex = threading.Lock()
+
     @staticmethod
     def __create_command_route_handler(command_handler, command_name: str):
         empty_query = Query([])
@@ -24,15 +27,16 @@ class HuemonServerFactory:  # pylint: disable=too-few-public-methods
         def handle_command_route(
             q: List[str] = empty_query,
         ):  # pylint: disable=invalid-name
-            context_reader = io.StringIO()
-            with contextlib.redirect_stdout(context_reader):
-                try:
-                    command_handler.exec(command_name, q)
-                except SystemExit as system_exit:
-                    raise HTTPException(
-                        detail=context_reader.getvalue(),
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                    ) from system_exit
+            with HuemonServerFactory.__stdout_reader_mutex:
+                context_reader = io.StringIO()
+                with contextlib.redirect_stdout(context_reader):
+                    try:
+                        command_handler.exec(command_name, q)
+                    except SystemExit as system_exit:
+                        raise HTTPException(
+                            detail=context_reader.getvalue(),
+                            status_code=status.HTTP_400_BAD_REQUEST,
+                        ) from system_exit
 
             return Response(context_reader.getvalue(), media_type="plain/text")
 
