38377c8f61858b2978eb5a34ebff02c905af646b
semantic-release <semantic-release>
2022-03-28 13:10:34 +0000

    chore: release 0.9.1
    Automatically generated by python-semantic-release

--- assets/release/release-docker.sh
+++ assets/release/release-docker.sh
@@ -7,7 +7,7 @@ function cd_to_root_directory () {
 function build_and_publish_docker_image () {
   cd_to_root_directory
 
-  echo "Building and publishing Docker image ghcr.io/edeckers/huemon:0.9.0"
-  docker buildx build . --platform linux/amd64,linux/arm64,linux/arm/v7 -t ghcr.io/edeckers/huemon:0.9.0 --push
-  echo "Built and published Docker image ghcr.io/edeckers/huemon:0.9.0"
+  echo "Building and publishing Docker image ghcr.io/edeckers/huemon:0.9.1"
+  docker buildx build . --platform linux/amd64,linux/arm64,linux/arm/v7 -t ghcr.io/edeckers/huemon:0.9.1 --push
+  echo "Built and published Docker image ghcr.io/edeckers/huemon:0.9.1"
 }
